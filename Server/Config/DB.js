d975ef155876e44bb311cb95d19915d2a0a00c89
ArJuN-888 <ksarjun635@gmail.com>
2024-03-08 11:16:43 +0530

    updated

--- Server/Config/DB.js
+++ Server/Config/DB.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const DatabaseCon = mongoose.connect("mongodb://127.0.0.1:27017/Chat")
+const DatabaseCon = mongoose.connect("mongodb+srv://schoolmanager:school123@schoolcluster.py2czfw.mongodb.net/school?retryWrites=true&w=majority&appName=Schoolcluster")
 .then(()=>console.log("Database Connected Successfully"))
 .catch((error)=>console.log("Unable to establish a connection",error))
 module.exports = {

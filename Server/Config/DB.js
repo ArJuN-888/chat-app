121a1480dc195e3df4a4e9681a01e87a2d68eed8
ArJuN-888 <ksarjun635@gmail.com>
2024-03-08 17:12:00 +0530

    updated

--- Server/Config/DB.js
+++ Server/Config/DB.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-const DatabaseCon = mongoose.connect("mongodb+srv://schoolmanager:school123@schoolcluster.py2czfw.mongodb.net/school?retryWrites=true&w=majority&appName=Schoolcluster")
+require("dotenv").config()
+const DatabaseCon = mongoose.connect(process.env.DB_URI)
 .then(()=>console.log("Database Connected Successfully"))
 .catch((error)=>console.log("Unable to establish a connection",error))
 module.exports = {
